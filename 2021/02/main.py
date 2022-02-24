e9ffbe4140dcaf7a75e74bc03f68aac2aec1bc07
Sailsman <danielgluch33@gmail.com>
2022-02-24 21:43:12 +0100

    2021-2, Python

---
+++ 2021/02/main.py
@@ -0,0 +1,52 @@
+def load_instructions(fname):
+    with open("2021/02/" + fname) as file:
+        instructions = file.read().split("\n")
+    return instructions
+
+
+def apply_action1(depth, horizontal, action):
+    operation, value = action.split(" ")
+    value = int(value)
+    if operation == "forward":
+        horizontal += value
+    elif operation == "down":
+        depth += value
+    elif operation == "up":
+        depth -= value
+    return depth, horizontal
+
+
+def part1():
+    instructions = load_instructions("instructions.txt")
+    depth = 0
+    horizontal = 0
+    for instruction in instructions:
+        depth, horizontal = apply_action1(depth, horizontal, instruction)
+    return depth * horizontal
+
+
+def apply_action2(depth, horizontal, aim, action):
+    operation, value = action.split(" ")
+    value = int(value)
+    if operation == "forward":
+        horizontal += value
+        depth += value * aim
+    elif operation == "down":
+        aim += value
+    elif operation == "up":
+        aim -= value
+    return depth, horizontal, aim
+
+
+def part2():
+    instructions = load_instructions("instructions.txt")
+    depth = 0
+    horizontal = 0
+    aim = 0
+    for instruction in instructions:
+        depth, horizontal, aim = apply_action2(depth, horizontal, aim, instruction)
+    return horizontal * depth
+
+if __name__ == "__main__":
+    # print(part1())
+    print(part2())
